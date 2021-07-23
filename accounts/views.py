3de323149b9424936489dde29b97560ccd20858b
Blake Holleman <blake.holleman@gmail.com>
2021-07-23 09:45:04 -0400

    add change user account info from home page

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from django.contrib.auth import login
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views.generic import UpdateView
+
 from .forms import SignUpForm
 
 # Create your views here.
@@ -15,3 +21,15 @@ def signup(request):
         form = SignUpForm()
         
     return render(request, 'signup.html', {'form': form})
+
+
+@method_decorator(login_required, name='dispatch')
+class UserUpdateView(UpdateView):
+    model = User
+    fields = ('first_name', 'last_name', 'email', )
+    template_name = 'my_account.html'
+    success_url = reverse_lazy('my_account')
+    context_object_name = 'user'
+    
+    def get_object(self):
+        return self.request.user
